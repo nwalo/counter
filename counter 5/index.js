857dfb70be50bdfe5a7706ef8a2050ca2f8b07a5
Nwalo Okechukwu <nwalookechukwu@gmail.com>
2021-08-07 12:36:01 +0100

    2nd commit

---
+++ counter 5/index.js
@@ -0,0 +1,15 @@
+$('.count').each(function () {
+
+
+    $(this).prop('counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+
+            // console.log(Math.ceil(now))
+        }
+    });
+});
